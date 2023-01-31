5c2e43f0b2cca6663565229677c5a0183d44341f
john-royal <34844819+john-royal@users.noreply.github.com>
2023-01-31 16:20:24 -0500

    Add Firebase

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,18 @@
+import { initializeApp } from 'firebase/app'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+
+initializeApp({
+  apiKey: 'AIzaSyAA6Nn11qNk_OMhY5EoTnsz1LgS75cnCQw',
+  authDomain: 'headstarter-tech-incubator.firebaseapp.com',
+  projectId: 'headstarter-tech-incubator',
+  storageBucket: 'headstarter-tech-incubator.appspot.com',
+  messagingSenderId: '460632280800',
+  appId: '1:460632280800:web:90de62867fd9ba06d34a85',
+  measurementId: 'G-NCLDD78YQM'
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
