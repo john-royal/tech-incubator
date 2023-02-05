ade6b7238527291ee37a750d66b2aaac3f4ee5a5
Masroor Khonkhodzhaev <masroor47@icloud.com>
2023-02-04 22:30:28 -0500

    started working on student profile page

--- src/pages/StudentProfile.tsx
+++ src/pages/StudentProfile.tsx
@@ -1,4 +1,5 @@
 import { doc, getDoc } from 'firebase/firestore'
+import { Button, Col, Container, Image, Row } from 'react-bootstrap'
 import { redirect, useLoaderData, type LoaderFunction } from 'react-router-dom'
 import { auth, db } from '../lib/firebase'
 import { type Student } from '../lib/types'
@@ -7,13 +8,20 @@ export default function StudentProfile (): JSX.Element {
   const student = useLoaderData() as Student
 
   return (
-    <>
-        <img src={student.imageURL} alt={student.name} />
-        <h1>{student.name}</h1>
-        <p>{student.bio}</p>
-        <p>Major: {student.major}</p>
-        <p>Year: {student.year}</p>
-    </>
+    <Container>
+      <Row className="my-5">
+        <Col xs={12} md={6}>
+          <Image src={student.imageURL} fluid />
+        </Col>
+        <Col xs={12} md={6}>
+          {/* <img src={student.imageURL} alt={student.name} /> */}
+          <h1>{student.name}</h1>
+          <p>{student.bio}</p>
+          <p>Major: {student.major}</p>
+          <p>Year: {student.year}</p>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
